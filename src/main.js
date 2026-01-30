7be41faed7ebc795bd43209b78aa4f9299c05d65
bedanavrvm <bedanaurum99@gmail.com>
2026-01-30 12:00:45 -0800

    Debug msg for VITE_API_BASE_URL

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import './styles/design.css'
 import App from './App.vue'
 import router from './router'
 
+// Debug: Log API base URL
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL)
+
 createApp(App).use(router).mount('#app')
